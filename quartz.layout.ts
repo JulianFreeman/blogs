81d7c8aff476638d0966fa82949b381b6cdee8cd
Julian Freeman <freemanjulian372@gmail.com>
2025-11-16 20:29:29 -0400

    Quartz sync: Nov 16, 2025, 8:29 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,7 +17,10 @@ export const sharedPageComponents: SharedLayout = {
 export const defaultContentPageLayout: PageLayout = {
     beforeBody: [
         Component.ConditionalRender({
-            component: Component.Breadcrumbs(),
+            component: Component.Breadcrumbs({
+                rootName: "主页",
+                showCurrentPage: false,
+            }),
             condition: (page) => page.fileData.slug !== "index",
         }),
         Component.ArticleTitle(),
@@ -48,7 +51,11 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-    beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+    beforeBody: [
+        // Component.Breadcrumbs(),
+        Component.ArticleTitle(), 
+        Component.ContentMeta()
+    ],
     left: [
         Component.PageTitle(),
         Component.MobileOnly(Component.Spacer()),
